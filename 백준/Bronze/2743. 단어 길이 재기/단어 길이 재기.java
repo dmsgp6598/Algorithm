ebc2e65acc6814e373a667c9bfd22647793045ea
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-03-28 13:40:57 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = bf.readLine();
+        
+        System.out.println(s.length());
+    }
+}
