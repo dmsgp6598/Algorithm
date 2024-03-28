38c421bdbbf7e89d876b25635030eb9c023cf087
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-03-28 13:32:43 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        if(a!=b && b!=c && a!=c) {
+            int max = Math.max(a, Math.max(b,c));
+            System.out.println(max*100);
+        }else if(a==b && b==c && a==c) {
+            System.out.println(10000+a*1000);
+        }else if(a==b || a==c) {
+            System.out.println(1000+a*100);
+        }else if(b==c) {
+            System.out.println(1000+b*100);
+        }
+        
+        bf.close();
+    }
+}
