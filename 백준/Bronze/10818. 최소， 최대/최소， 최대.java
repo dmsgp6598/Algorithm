70c7b7ffab247de0a21e66b2665742e8c82952b9
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-03-28 13:38:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 1356 ms, Memory: 109452 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(bf.readLine());
+        int[] arr = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        bf.close();
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N-1]);
+            
+    }
+}
