98cd94688db4bbc064a0082525660dd2d4de01cc
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-03-28 13:29:20 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder bw = new StringBuilder();
+        
+        bw.append("|\\_/|\n");
+        bw.append("|q p|   /}\n");
+        bw.append("( 0 )\"\"\"\\\n");
+        bw.append("|\"^\"`    |\n");
+        bw.append("||_/=\\\\__|\n");
+        
+        System.out.println(bw);
+    }
+}
