e9664272860c8292e6a9bd8d007145263362935d
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-03-28 13:41:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 276 ms, Memory: 19988 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        
+        System.out.println(st.countTokens());
+        bf.close();
+        
+    }
+}
