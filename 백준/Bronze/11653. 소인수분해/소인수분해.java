76ac68220782cee2054883fed88bbc4e76eb6e1f
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-03-28 14:12:27 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=2; i*i<=N; i++) {
+            while(N % i == 0) {
+                sb.append(i).append('\n');
+                N /= i;
+            }
+        }
+        if(N != 1) sb.append(N);
+        
+        System.out.println(sb);
+    }
+}
