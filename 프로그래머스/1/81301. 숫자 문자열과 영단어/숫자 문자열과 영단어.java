d9d4a60b1eb3c35a5b9e1195786f7f2b08f87d03
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-04-07 17:09:37 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,64 +1,13 @@
 class Solution {
-    public int solution(String s) {
-        StringBuilder sb = new StringBuilder();
+    public static int solution(String s) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        char[] ch = s.toCharArray();
-        int idx = 0;
-        
-        while(idx <= s.length()-1) {
-            switch(ch[idx]) {
-                case 'z' : 
-                    sb.append(0); 
-                    idx += 4;
-                    break;
-                case 'o' : 
-                    sb.append(1); 
-                    idx += 3;
-                    break;
-                case 't' : 
-                    if(ch[idx+1] == 'w') {
-                        sb.append(2);
-                        idx += 3;
-                        break;
-                    }else {
-                        sb.append(3);
-                        idx += 5;
-                        break;
-                    }
-                case 'f' : 
-                    if(ch[idx+1] == 'o') {
-                        sb.append(4);
-                        idx += 4;
-                        break;
-                    }else {
-                        sb.append(5); 
-                        idx += 4;
-                        break;
-                    }
-                case 's' : 
-                    if(ch[idx+1] == 'i') {
-                        sb.append(6); 
-                        idx += 3;
-                        break;
-                    }else {
-                        sb.append(7); 
-                        idx += 5;
-                        break;
-                    }
-                case 'e' : 
-                    sb.append(8); 
-                    idx += 5;
-                    break;
-                case 'n' : 
-                    sb.append(9); 
-                    idx += 4;
-                    break;
-                default : 
-                	sb.append(ch[idx]); 
-                	idx++;
-                	break; 
+        for (int i = 0; i < arr.length; i++) {
+            if (s.contains(arr[i])) {
+                s = s.replaceAll(arr[i], Integer.toString(i));
             }
         }
-        return Integer.parseInt(sb.toString());
+        
+        return Integer.parseInt(s);
     }
 }
