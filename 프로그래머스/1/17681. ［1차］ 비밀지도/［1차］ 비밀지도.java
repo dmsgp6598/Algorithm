3514a939ab336613035adf0ee19a6fbef16b8745
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-04-08 23:17:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.15 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,15 +1,16 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        
-        for(int i=0; i<n; i++) {
-            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
-            binary = String.format("%" + n + "s", binary);
-            binary = binary.replace("0", " ").replace("1", "#");
-            answer[i] = binary;
-            
-        }
-        
-        return answer;
+        String temp;
+
+        for(int i = 0 ; i < n ; i++){
+          temp = String.format("%16s", Integer.toBinaryString(arr1[i] | arr2[i]));
+          temp = temp.substring(temp.length() - n);
+          temp = temp.replaceAll("1", "#");
+          temp = temp.replaceAll("0", " ");
+          answer[i] = temp;
+      }
+
+      return answer;
     }
 }
