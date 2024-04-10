e096e792b79123620db27babfebaa243bf86558b
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-04-10 10:36:14 +0900

    [level 1] Title: 폰켓몬, Time: 3.31 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,17 @@
-import java.util.*;
+import java.util.HashSet;
+
 class Solution {
     public int solution(int[] nums) {
-        //1. 기존 length를 구한다.
-        //2. 중복값을 제거한 length를 구한다.
-        //3. 두 값중 최소값이 정답.
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i = 0 ; i < nums.length; i++){
-            if(!list.contains(nums[i])){
-                list.add(nums[i]);
-            }
+        int max = nums.length / 2;
+        
+        HashSet<Integer> hset = new HashSet<>();
+        
+        for(int n : nums) {
+            hset.add(n);
         }
-
-        return nums.length/2 > list.size()?list.size():nums.length/2;
+        
+        if(max >= hset.size()) return hset.size();
+        else return max;
+        
     }
 }
