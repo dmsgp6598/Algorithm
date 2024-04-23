484991b21b64d5d57c30b04e1e9d348ea9b27042
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-04-23 10:51:44 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.30 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,27 +1,25 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
         StringBuilder sb = new StringBuilder();
-
+        
         int left = 10;
         int right = 12;
-
-        for (int i = 0; i < numbers.length; i++) {
-            int n = numbers[i];
-
-            if (n == 1 || n == 4 || n == 7) {
+        
+        for(int n : numbers) {
+            if(n == 1 || n == 4 || n == 7) {
                 left = n;
                 sb.append("L");
             }
-            if (n == 3 || n == 6 || n == 9) {
+            if(n == 3 || n == 6 || n == 9) {
                 right = n;
                 sb.append("R");
             }
-            if (n == 2 || n == 5 || n == 8 || n == 0) {
-                if( n == 0 ) n = 11;
-
-                int leftDiff = (Math.abs(n - left) / 3) + (Math.abs(n - left) % 3);
-                int rightDiff =(Math.abs(n - right) / 3) + (Math.abs(n - right) % 3);
-
+            if(n == 2 || n == 5 || n == 8 || n == 0) {
+                if(n == 0) n = 11;
+                
+                int leftDiff = (Math.abs(n-left) / 3) + (Math.abs(n-left) % 3);
+                int rightDiff = (Math.abs(n-right) / 3) + (Math.abs(n-right) % 3);
+                
                 if (leftDiff == rightDiff) {
                     if (hand.equals("right")) {
                         right = n;
@@ -39,6 +37,7 @@ class Solution {
                 }
             }
         }
+        
         return sb.toString();
     }
 }
