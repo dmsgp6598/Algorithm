a030d2d6847f072ef474976653fdea1a780c7dfb
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-04-24 23:39:49 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 7.37 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    int sort_by_Num = 0;
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        String[] dataArr = {"code","date","maximum","remain"};
+        int extNum = 0;
+        
+        for(int i =0; i<dataArr.length; i++) {
+            if(dataArr[i].equals(ext)) {
+                extNum = i;
+            }
+            if(dataArr[i].equals(sort_by)) {
+                sort_by_Num = i;
+            }
+        }
+        
+        List<Integer[]> dataList = new ArrayList<>();
+        for(int i =0; i<data.length; i++) {
+            if(data[i][extNum] < val_ext) {
+                dataList.add(Arrays.stream(data[i]).boxed().toArray(Integer[]::new));
+            }
+        }
+        
+        Collections.sort(dataList, (arr1, arr2) -> {
+            return Integer.compare(arr1[sort_by_Num], arr2[sort_by_Num]);
+            
+        });
+
+        int[][] answer = new int[dataList.size()][dataList.get(0).length];
+        for (int i = 0; i < dataList.size(); i++) {
+            answer[i] = Arrays.stream(dataList.get(i)).mapToInt(Integer::intValue).toArray();
+        }
+        
+        return answer;
+    }
+}
