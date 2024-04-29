ae43774f7794acb556602ac9805aad0165ca0f0e
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-04-29 18:40:26 +0900

    [level 1] Title: 달리기 경주, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -6,18 +6,13 @@ class Solution {
         int numOfPlayers = players.length;
         Map<String, Integer> ranking = new HashMap<>();
         
-        for (int i=0; i<numOfPlayers ; i++) {
+        for (int i=0; i<numOfPlayers; i++) {
             ranking.put(players[i], i);
         }
         
-
-        
-        //경주 진행
         for (String player : callings) {
-            //1) player의 이름에 해당하는 value를 저장한다. 
             int playerRanking = ranking.get(player);
             
-            //2) player보다 앞에 있는 사람을 발견하고, value를 변경함
             String frontPlayer = players[playerRanking-1];
             
             ranking.replace(frontPlayer, playerRanking);
