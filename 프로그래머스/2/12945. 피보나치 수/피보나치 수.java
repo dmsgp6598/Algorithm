89d619cb7414fe78057471c83c16ac2da291d3a4
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-05-09 17:07:00 +0900

    [level 2] Title: 피보나치 수, Time: 3.26 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i=2; i<=n; i++) {
+            arr[i] = (arr[i-1] + arr[i-2])%1234567;
+        }
+        
+        
+        return arr[n];
+    }
+    
+    private static int Fibo(int num) {
+        if(num <= 1) return num;
+        
+        return Fibo(num-1) + Fibo(num-2);
+    }
+}
