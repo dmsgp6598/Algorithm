1be2de12eec302f4e661eb6abe488325878f1625
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-05-10 15:21:29 +0900

    [level 2] Title: 짝지어 제거하기, Time: 53.52 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        char[] arr = s.toCharArray();
+		Stack<Character> stack = new Stack<>();
+		
+		for(char ch : arr) {
+			if(stack.isEmpty()) {
+				stack.push(ch);
+				continue;
+			} else {
+				if(stack.peek() == ch) {
+					stack.pop();
+				}
+				else stack.push(ch);
+			}
+		}
+        
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
