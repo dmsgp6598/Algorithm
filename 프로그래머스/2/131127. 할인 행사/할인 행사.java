68fe426d407c76461302bbeef2a874ea202bab81
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-05-13 15:24:11 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+		int days = 10;
+		
+		HashMap<String, Integer> hmap = new HashMap<>();
+		
+		for(int i=0; i<want.length; i++) {
+			hmap.put(want[i], number[i]);
+		}
+		
+		for(int i=0; i<discount.length - days + 1; i++) {
+			HashMap<String, Integer> dmap = new HashMap<>();
+			
+			for(int j=0; j<days; j++) {
+				dmap.put(discount[i + j], dmap.getOrDefault(discount[i + j], 0)+1);
+			}
+			
+			Boolean chk = true;
+			
+			for(String key : dmap.keySet()) {
+				if(hmap.get(key) != dmap.get(key)) {
+					chk = false;
+					break;
+				}
+			}
+			answer += chk ? 1 :0;
+		}
+		
+        return answer;
+    }
+}
