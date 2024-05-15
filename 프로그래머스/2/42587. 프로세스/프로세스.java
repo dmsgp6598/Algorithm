00dc905f2a46d69fa60f722733774f8f49692074
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-05-16 00:16:17 +0900

    [level 2] Title: 프로세스, Time: 0.77 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int n : priorities) {
+            q.offer(n);
+        }
+        while(!q.isEmpty()) {
+            for(int i=0; i<priorities.length; i++) {
+                if(priorities[i] == q.peek()) {
+                    q.poll();
+                    answer++;
+                    if(i == location) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
