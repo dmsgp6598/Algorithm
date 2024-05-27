5a2cf0fdb31f2782ea9d70b4d64f8848c6539e6a
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-05-27 16:18:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.99 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,14 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-// start 1:14
-
 class Solution {
     static int answer = -1;
-    static boolean[][] visited;
-    static int[] mrow = {-1, 1, 0, 0};
-    static int[] mcol = {0, 0, -1, 1};
-    
+	static boolean[][] visited;
+	static int[] mrow = {-1, 1, 0 , 0};
+	static int[] mcol = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
         visited = new boolean[maps.length][maps[0].length];
@@ -19,49 +16,47 @@ class Solution {
     }
     
     public void bfs(int row, int col, int[][] maps) {
-        Queue<Node> q = new LinkedList<>();
-        
-        q.add(new Node(0, 0, 1));
-        visited[0][0] = true;
-        
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            
-
-        // System.out.printf("row=%d, col=%d, count = %d\n", cur.row, cur.col, cur.count);
-
-            if (cur.row == maps.length - 1 && cur.col == maps[0].length - 1) {
-                answer = cur.count;
-                // System.out.println("catch");
-                return;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nextRow = cur.row + mrow[i];
-                int nextCol = cur.col + mcol[i];
-                
-                if (canMove(nextRow, nextCol, maps)) {
-                    // System.out.printf("add row=%d, col=%d, count = %d\n", cur.row, cur.col, cur.count);
-                    visited[nextRow][nextCol] = true;
-                    q.add(new Node(nextRow, nextCol, cur.count + 1));
-                }
-                
-            }
-        }
-    }
+		Queue<Node> q = new LinkedList<>();
+		q.add(new Node(0, 0, 1));
+		
+		visited[0][0] = true;
+		
+		while(!q.isEmpty()) {
+			Node cur = q.poll();
+			
+			if(cur.row == maps.length-1 && cur.col == maps[0].length-1) {
+				answer = cur.cnt;
+				return;
+			}
+			
+			for(int i=0; i<4; i++) {
+				int nextRow = cur.row + mrow[i];
+				int nextCol = cur.col + mcol[i];
+				
+				if(chkNode(nextRow, nextCol, maps)) {
+					visited[nextRow][nextCol] = true;
+					q.add(new Node(nextRow, nextCol, cur.cnt+1));
+				}
+				
+			}
+			
+		}
+		
+	}
     
     class Node {
-        int row;
-        int col;
-        int count;
-        
-        public Node(int row, int col, int count) {
-            this.row = row;
-            this.col = col;
-            this.count = count;
-        }
-    }
+		int row;
+		int col;
+		int cnt;
+		
+		public Node(int row, int col, int cnt) {
+			this.row = row;
+			this.col = col;
+			this.cnt = cnt;
+		}
+	}
     
-    public boolean canMove(int row, int col, int[][] maps) {
+    public boolean chkNode(int row, int col, int[][] maps) {
         return row >= 0 && row < visited.length && col >= 0 && col < visited[0].length
                     && !visited[row][col] && maps[row][col] != 0;
     }
