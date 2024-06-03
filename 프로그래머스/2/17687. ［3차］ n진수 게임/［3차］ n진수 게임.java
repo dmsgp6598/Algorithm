6b67b02f524a8335e195dbcb5406ac0d6ef7ec58
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-06-03 15:20:38 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.44 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,16 +1,15 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder convert = new StringBuilder();
         StringBuilder answer = new StringBuilder();
         
-        for(int i = 0; convert.length() <= t * m; i++){
-            convert.append(Integer.toString(i, n));
+        int num = 0;
+        while(convert.length() < t * m) {
+        	convert.append(Integer.toString(num++, n));
         }
         
-        for(int i = p - 1;  answer.length() < t; i += m){
-            answer.append(convert.charAt(i));
+        for(int i=0; i<t; i++) {
+        	answer.append(convert.charAt(p-1+i*m));
         }
         
         return answer.toString().toUpperCase();
