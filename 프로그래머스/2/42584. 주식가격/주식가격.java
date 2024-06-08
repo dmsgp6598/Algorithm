358f091d4a3282b2c8ad0db0f91b29597c80d222
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-06-08 21:36:30 +0900

    [level 2] Title: 주식가격, Time: 25.07 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,23 +1,29 @@
 import java.util.Stack;
 
-
 class Solution {
-	public int[] solution(int[] prices) {
+    public static void main(String[] args) {
+		int[] prices = {1, 2, 3, 2, 3};
+		System.out.println(solution(prices));
+	}
+	
+	public static int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
-		Stack<Integer> stack = new Stack<>();
-
-		for (int i = 0; i < prices.length; i++) {
-			while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(int i=0; i<prices.length; i++) {
+			while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
 				answer[stack.peek()] = i - stack.peek();
 				stack.pop();
 			}
 			stack.push(i);
 		}
-
-		while (!stack.isEmpty()) {
-			answer[stack.peek()] = prices.length - stack.peek() - 1;
+		
+		while(!stack.isEmpty()) {
+			answer[stack.peek()] = prices.length - stack.peek() -1;
 			stack.pop();
 		}
-		return answer;
+		
+        return answer;
 	}
 }
