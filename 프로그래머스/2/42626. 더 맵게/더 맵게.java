510c821d215513a51827ff77adc38f3a37c88794
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-06-08 22:11:54 +0900

    [level 2] Title: 더 맵게, Time: 1645.92 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,19 +1,26 @@
 import java.util.PriorityQueue;
 
 class Solution {
-    public int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> que = new PriorityQueue<>();
+    public void main(String[] args) {
+		int[] scoville = {1, 2, 3, 9, 10, 12};
+		int K = 7;
+		System.out.println(solution(scoville, K));
+	}
+	
+	public int solution(int[] scoville, int K) {
+		int answer = 0;
+        PriorityQueue<Integer> que = new PriorityQueue<Integer>();
         
-        for (int i = 0; i < scoville.length; i++) {
-            que.add(scoville[i]);
+        for(int i=0; i<scoville.length; i++) {
+        	que.offer(scoville[i]);
         }
+        
         while(que.peek() < K) {
-            if (que.size() == 1)
-                return -1;
-            que.add(que.poll() + que.poll() * 2);
-            answer++;
+        	if(que.size() == 1) return -1;
+        	
+        	que.offer(que.poll() + que.poll() * 2);
+        	answer++;
         }
-        return answer;
-    }
+		return answer;
+	}
 }
