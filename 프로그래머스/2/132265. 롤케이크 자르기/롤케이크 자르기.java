659607dfe4fa6bace85bca1e4d35b19559f90b59
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-06-09 21:45:34 +0900

    [level 2] Title: 롤케이크 자르기, Time: 171.16 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,24 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Set<Integer> set = new HashSet<Integer>();
         
-        for(int i = 0; i < topping.length; i++) {
-            map.put(topping[i], map.getOrDefault(topping[i], 0) + 1);
+        for(int i=0; i<topping.length; i++) {
+        	map.put(topping[i], map.getOrDefault(topping[i], 0) + 1);
         }
         
-        for(int i = 0; i < topping.length; i++) {
-            set.add(topping[i]);
-            map.put(topping[i], map.getOrDefault(topping[i], 0) - 1);
-            if(map.get(topping[i]) == 0) {
-                map.remove(topping[i]);
-            }
-            
-            if(set.size() == map.size()) {
-                answer++;
-            }
+        for(int i=0; i<topping.length; i++) {
+        	set.add(topping[i]);
+        	map.put(topping[i], map.getOrDefault(topping[i], 0) - 1);
+        	
+        	if(map.get(topping[i]) == 0) map.remove(topping[i]);
+        	
+        	if(set.size() == map.size()) answer++;
+        	
         }
+       
         return answer;
     }
 }
