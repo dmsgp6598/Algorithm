6740eb45e61b7b0421bb178d323ccfda93e82536
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-06-10 20:11:42 +0900

    [level 2] Title: 방문 길이, Time: 17.86 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,44 +1,42 @@
-import java.util.*;
- 
-public class Solution {
-    public static int solution(String dirs) {
-        int answer = 0;
- 
-        HashSet<String> visited = new HashSet<>();
- 
-        int currentX = 0;
-        int currentY = 0;
- 
-        for (int i = 0; i < dirs.length(); i++) {
-            int nextX = currentX;
-            int nextY = currentY;
-            String moving = "";
- 
-            if (dirs.charAt(i) == 'U') {
-                nextY++; // 위로 한칸
-                moving += currentX + "" + currentY + "->" + nextX +  nextY;
-            } else if (dirs.charAt(i) == 'D') {
-                nextY--; // 밑으로 한칸
-                moving += nextX + "" + nextY + "->" + currentX + currentY ;
-            } else if (dirs.charAt(i) == 'R') {
-                nextX++; // 오른쪽으로 한칸
-                moving += currentX + "" + currentY + "->" + nextX + nextY;
-            } else {
-                nextX--; // 왼쪽으로 한칸
-                moving += nextX + "" + nextY + "->" + currentX + currentY;
-            }
- 
-            // 좌표평면의 경계를 넘어가면 추가하지 않고 다음 명령으로 넘어간다.
-            if (nextX < -5 || nextY < -5 || nextX > 5 || nextY > 5) {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public int solution(String dirs) {
+        Map<String, int[]> map = new HashMap<>();
+        map.put("U", new int[] {0, 1});
+        map.put("D", new int[] {0, -1});
+        map.put("R", new int[] {1, 0});
+        map.put("L", new int[] {-1, 0});
+
+        String[] arr = dirs.split("");
+
+        Set<String> set = new HashSet<>();        
+        int cx = 0;
+        int cy = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            String s = arr[i];
+
+            int x = map.get(s)[0];
+            int y = map.get(s)[1];
+
+            cx += x;
+            cy += y;
+
+            if(cx < -5 || cx > 5) {
+                cx -= x;
                 continue;
             }
-            visited.add(moving);
- 
-            currentX = nextX;
-            currentY = nextY;
-        }
-        answer = visited.size();
- 
-        return answer;
+            if(cy < -5 || cy > 5) {
+                cy -= y;
+                continue;
+            }
+            set.add(""+(cx-x)+ ""+ (cy-y)+ ""+cx + ""+cy);
+            set.add(""+cx + ""+cy+""+(cx-x)+ ""+ (cy-y));
+        } 
+        return set.size()/2;
     }
 }
