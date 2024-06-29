1ec7e18314f66d7936f4d06e2931d854075be3b7
dmsgp6598 <74399236+dmsgp6598@users.noreply.github.com>
2024-06-29 23:25:56 +0900

    [Bronze I] Title: 약수, Time: 112 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
 import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
